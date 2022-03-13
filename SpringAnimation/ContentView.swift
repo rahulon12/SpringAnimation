840a7418539f58f965fa190b65e84a9080af696e
rahulon12 <rahulon@icloud.com>
2022-03-13 18:37:17 -0400

    Initial Commit

---
+++ SpringAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpringAnimation
+//
+//  Created by Rahul Narayanan on 3/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpringAnimation/SpringAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringAnimationApp.swift
+//  SpringAnimation
+//
+//  Created by Rahul Narayanan on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SpringAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
